0425fc0c5d80e35e15f37c5a1695065ab111e36a
karabanb <b.karaban@me.com>
2019-05-12 20:21:35 +0200

    init commit

---
+++ wprowadzenie_zadania.py
@@ -0,0 +1,15 @@
+# Zadanie 1. 1. 1 obrazek
+
+print('  _______  \n@/  . .  \\@\n\n(  \\___/  )\n\n \\_______/')
+
+# Zadanie 1. 1. 2
+
+a = 55
+b = 10
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a % b)
+
