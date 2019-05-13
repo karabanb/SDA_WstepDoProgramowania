b8b8a275c8275fc4ae773955c33d3e6ad5a7948d
karabanb <b.karaban@me.com>
2019-05-13 18:34:10 +0200

    zadania od 1.1.3 do 1.1.7

--- wprowadzenie_zadania.py
+++ wprowadzenie_zadania.py
@@ -13,3 +13,35 @@ print(a*b)
 print(a/b)
 print(a % b)
 
+# zadanie 1. 1. 3
+
+print(f'a + b = {a + b}')
+print(f'a - b = {a - b}')
+print(f'a * b = {a * b}')
+print(f'a / b = {a / b}')
+print(f'a % b = {a % b}')
+
+# zadanie 1. 1. 4
+
+radius = input('Podaj promień:')
+obwod = int(radius) * 2 * 3.14
+print(f'Obwód to: {obwod}')
+
+# zadanie 1. 1. 5
+
+hours = input('Podaj liczbę godzin:')
+seconds = int(hours)*3600
+print(f'Jest to: {seconds}')
+
+# zadanie 1. 1. 6
+
+celsius = input('Podaj liczbę stopni celsjusza:')
+kelwin = int(celsius)+273.15
+fahrenheit = 32 + (9/5 * int(celsius))
+
+print(f'{celsius} stopni w C to : {kelwin} K\n'
+      f'{celsius} stopni w C to : {fahrenheit} F')
+
+# zadanie 1. 1. 7
+
+
