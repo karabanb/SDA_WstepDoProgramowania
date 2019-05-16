5dc48175aeb37433d5526cacd091542961f9ed86
karabanb <b.karaban@me.com>
2019-05-16 22:19:08 +0200

    zadanie 2.1.1 do 2.1.5

--- wprowadzenie_zadania.py
+++ wprowadzenie_zadania.py
@@ -82,3 +82,49 @@ if __name__ == '__main__':
         print('ok')
     else:
         print('niedowaga')
+
+# zadaie 2. 1. 1
+
+    c = 0
+
+    while c <= 100:
+        print(c)
+        c += 7
+
+# zadanie 2. 1. 2
+
+    number = int(input('Podaj dowolną liczbę:'))
+
+    while number <= 1000:
+        number = number * 2
+        print(number)
+
+# zdanie 2. 1. 3
+
+    star = '*'
+    while len(star) < 5:
+        print(star)
+        star = star + '*'
+
+    for i in range(5):
+        print('*' * i)
+
+# zadanie 2. 1. 4
+
+    while True:
+        user_answer = str(input('Czy chcesz zakończyć pracę?:'))
+        if user_answer == 'y':
+            break
+# zadanie 2. 1. 5
+
+    celsius = input('Podaj liczbę stopni celsjusza:')
+    k_or_f = []
+
+    while k_or_f not in ['k', 'f']:
+        k_or_f = str(input('Przeliczyć na Kelwiny czy Fahrenheity? (k/f):'))
+        if k_or_f == 'k':
+            kelwin = int(celsius) + 273.15
+            print(f'{celsius} stopni C to : {kelwin} K')
+        else:
+            fahrenheit = 32 + (9 / 5 * int(celsius))
+            print(f'{celsius} stopni C to : {fahrenheit} F')
