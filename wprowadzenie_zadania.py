6edc0d30cd7c4c3064b52a59bb99701db88dcb2c
karabanb <b.karaban@me.com>
2019-05-17 23:36:31 +0200

    3. 1. 6

--- wprowadzenie_zadania.py
+++ wprowadzenie_zadania.py
@@ -267,6 +267,16 @@ if __name__ == '__main__':
         for j in range(n):
             print(string)
 
+    # zadanie 3. 1. 6
+
+    def leap_year(rok):
+        years = []
+        year = rok
+        while len(years) < 10:
+            if (year % 4 == 0) and (year % 100 != 0) or (year % 400 == 0):
+                years.append(year)
+            year += 1
+        return years
 
 
 
