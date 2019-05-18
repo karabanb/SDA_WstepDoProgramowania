bdd610b8e8c4e7b3ae0067eedb30bb3512b26f6b
karabanb <b.karaban@me.com>
2019-05-18 10:55:09 +0200

    3. 1. 6

--- first_program.py
+++ first_program.py
@@ -218,6 +218,102 @@ for i in range(2, 101, 2):
 
 print(res)
 
+# słowniki
 
+user_data = {
+        'id': 1,
+        'first_name': 'John',
+        'last_name': 'Smith'
+    }
+
+print(user_data['id'])
+
+id_str = 'id'                   # klucz jako string
+
+user_data['id']                 # mozemy sie dobrac wprost przez klucz
+user_data[id_str]               # mozemy sie tez dobrac przez wartosc klucza zadeklarowana w zmiennej
+user_data['asd']                # wyzrzuca blad jak nie ma klucza w slowniku
+
+user_data.get('id')             # mozemy tez uzyc metody 'get' dla slownika
+user_data.get('asd')            # nie wyrzuci bledu gdy nie ma klucza w slowniku
+
+n = user_data.get('asd')
+type(n)                         # brak wartosci w sliwniku zwroci 'None'
+
+a = user_data.get('asd', 'a')   # mozna zadeklarowac wartosc domyslna, ktora zostanie zwrocona zamiast 'None'
+
+
+def name_as_list(data):
+    string = data.get('name')
+    return string.split(' ')
+
+
+data = {'name': 'Józef Kowalski'}
+
+my_list = name_as_list(data)    # wprzypdaku pustej listy wyrzuci blad
+print(my_list)
+
+data2 = {}
+
+my_list2 = name_as_list(data2)
+
+
+def name_as_list_better(data):
+    string = data.get('name', '')   # deklarujemy co ma zwrocic gdy nie ma klucza, lub lista jest pusta
+    return string.split(' ')
+
+
+my_list2 = name_as_list_better(data2)    # tym razem bedzie ok
+print(my_list2)
+
+
+def calculate_price(base_price, data):
+    percentage = data.get('price_percentage')
+    if percentage is not None:
+        new_price = base_price * percentage
+    else:
+        new_price = base_price
+    return new_price
+
+
+def calculate_price_better(base_price, data):
+    percentage = data.get('price_percentage', 1)
+    return base_price * percentage
+
+
+invoice_data = {
+    'id': 1,
+    'name': 'Bill Gates',
+    'address': {
+        'city': 'Redmont',
+        'country': 'US'
+    },
+    'price_percentage': 1.2
+}
+
+tshirt_price = 16
+price_for_bill_gates = calculate_price(tshirt_price, invoice_data)
+print(price_for_bill_gates)
+
+invoice_data['price_percentage'] = 2.7  # mozemy modyfikowac wartosci dla klucza
+invoice_data['price'] = 16              # jezeli nie ma klucza to zostanie utworzony nowy
+
+invoice_data.pop('price')               # usuwa klucz-wartosc ze slownika
+
+example_dict_in_dict = {'data': [       # slownik w slowniku
+    {
+        'id': 1,
+        'name': 'John Smith'
+    },
+    {
+        'id': 2,
+        'name': 'Merry Sue'
+    }
+]}
+
+# indeksowanie wewnatrz zagnieżdżonych słowników
+
+example_dict_in_dict['data'][0]
+example_dict_in_dict['data'][0]['name']
 
 
