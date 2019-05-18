ff1bd9dc6435b34f7bd4c5b4eb21dcbdd5a1707d
karabanb <b.karaban@me.com>
2019-05-18 12:46:48 +0200

    init commit

---
+++ biblioteki_i_import.py
@@ -0,0 +1,18 @@
+
+import math
+
+# funkcje wbudowane
+
+print('a')
+print(type(str(1)))
+
+# import modułów biblioteki standardowej
+
+num_pi = math.pi
+print(num_pi)
+
+# import wybranej funkcji z modułu
+
+from math import pi
+
+num_pi2 = pi
