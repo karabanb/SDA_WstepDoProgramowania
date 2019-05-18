296ec693c977c50365bf37a1dbad4e1c836f7107
karabanb <b.karaban@me.com>
2019-05-18 14:28:53 +0200

    zmiana miejsce '__name__'

--- erastothenes_sieve.py
+++ erastothenes_sieve.py
@@ -1,18 +1,24 @@
-if __name__ == "__main__":
 
-    def erastothenes_sieve(n):
-        if not n > 1:
-            print('Bad data@')
-            return
-        sieve = []
-        for i in range(n):
-            sieve.append(True)
-        for i in range(2, n // 2):
-            if sieve[i]:
-                for j in range(2 * i, n, i):
-                    sieve[j] = False
+
+def erastothenes_sieve(n):
+    if not n > 1:
+        print('Bad data@')
+        return
+    sieve = []
+    for i in range(n):
+           sieve.append(True)
+    for i in range(2, n // 2):
+        if sieve[i]:
+            for j in range(2 * i, n, i):
+                sieve[j] = False
         result_list = []
-        for i in range(2, n):
-            if sieve[i]:
+    for i in range(2, n):
+        if sieve[i]:
                 result_list.append(i)
-        return result_list
+    return result_list
+
+
+if __name__ == "__main__":
+    prime_numbers = erastothenes_sieve(100)
+    print(prime_numbers)
+
